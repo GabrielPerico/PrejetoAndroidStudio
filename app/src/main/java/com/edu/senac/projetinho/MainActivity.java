8161b013362ad19b482646cc25c68306f1bd7ac0
Gabriel <gabriel.perico@aluno.sc.senac.br>
2019-07-24 21:13:15 -0300

    primeiro commit

---
+++ app/src/main/java/com/edu/senac/projetinho/MainActivity.java
@@ -0,0 +1,72 @@
+package com.edu.senac.projetinho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+
+    EditText edtSenha,edtEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("Ciclo","Passando pelo método onCreate ...");
+        setContentView(R.layout.activity_main);
+
+        edtSenha=findViewById(R.id.edtSenha);
+        edtEmail=findViewById(R.id.edtEmail);
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.i("Ciclo", "Passando pelo método onStart ...");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        Log.i("Ciclo", "Passando pelo método onRestart ...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Ciclo", "Passando pelo método onStop ...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Ciclo", "Passando pelo método onResume ...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Ciclo", "Passando pelo método onPause ...");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Ciclo", "Passando pelo método onDestroy ...");
+    }
+
+    public void irParaPrincipal(View v){
+        String email=edtEmail.getText().toString(),senha=edtSenha.getText().toString();
+        if(email.equals("aluno@senac.br") && senha.equals("1234")) {
+            Intent i = new Intent(this, Principal.class);
+            startActivity(i);
+        }else{
+            Toast.makeText(this, "Usuario ou senha incorretos", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/edu/senac/projetinho/Principal.java
@@ -0,0 +1,33 @@
+package com.edu.senac.projetinho;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void irParaCalculo(View v){
+        Intent i = new Intent(this, CalculoIMC.class);
+        startActivity(i);
+    }
+
+    public void irParaConversao(View v){
+        Intent i = new Intent(this,Conversao.class);
+        startActivity(i);
+    }
+}
