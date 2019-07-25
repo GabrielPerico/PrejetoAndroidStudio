e8771eda97fe56db89c34170ad8c65ff497cabac
Gabriel <gabriel.perico@aluno.sc.senac.br>
2019-07-24 22:06:33 -0300

    Começado códigos para conversão

--- app/src/main/java/com/edu/senac/projetinho/Conversao.java
+++ app/src/main/java/com/edu/senac/projetinho/Conversao.java
@@ -9,8 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Conversao extends AppCompatActivity {
+
+    Spinner converterDe,converterPara;
+    EditText edtNumero;
+    TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,57 @@ public class Conversao extends AppCompatActivity {
         setContentView(R.layout.activity_conversao);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        edtNumero=findViewById(R.id.edtNumero);
+        converterDe=findViewById(R.id.converterDe);
+        converterPara=findViewById(R.id.converterPara);
+        textResultado=findViewById(R.id.textResultado);
+    }
+
+    public void converterNum(View v){
+        String cnvtDe=converterDe.getSelectedItem().toString(),cnvtPara=converterPara.getSelectedItem().toString();
+        Integer edtNum=Integer.parseInt(edtNumero.getText().toString());
+        if (cnvtDe == "KB"){
+            if (cnvtPara == "KB"){
+                textResultado.setText(Integer.toString((edtNum)));
+            }else if(cnvtPara == "MB"){
+                textResultado.setText(Integer.toString((edtNum/1024)));
+            }else if(cnvtPara == "GB"){
+                textResultado.setText(Integer.toString((edtNum/1048576)));
+            }else{
+                textResultado.setText(Integer.toString((edtNum/1073741824)));
+            }
+        }else if(cnvtDe == "MB"){
+            if (cnvtPara == "KB"){
+                textResultado.setText(Integer.toString((edtNum*1024)));
+            }else if(cnvtPara == "MB"){
+                textResultado.setText(Integer.toString((edtNum)));
+            }else if(cnvtPara == "GB"){
+                textResultado.setText(Integer.toString((edtNum/1024)));
+            }else{
+                textResultado.setText(Integer.toString((edtNum/1048576)));
+            }
+        }else if(cnvtDe == "GB"){
+            if (cnvtPara == "KB"){
+                textResultado.setText(Integer.toString(((edtNum*1048576))));
+            }else if(cnvtPara == "MB"){
+                textResultado.setText(Integer.toString((edtNum*1024)));
+            }else if(cnvtPara == "GB"){
+                textResultado.setText(Integer.toString((edtNum)));
+            }else{
+                textResultado.setText(Integer.toString((edtNum/1024)));
+            }
+        }else{
+            if (cnvtPara == "KB"){
+                textResultado.setText(Integer.toString((edtNum*1073741824)));
+            }else if(cnvtPara == "MB"){
+                textResultado.setText(Integer.toString((edtNum*1048576)));
+            }else if(cnvtPara == "GB"){
+                textResultado.setText(Integer.toString((edtNum*1024)));
+            }else{
+                textResultado.setText(Integer.toString((edtNum)));
+            }
+        }
     }
 
 }
